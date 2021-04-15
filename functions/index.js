5878b1c84df8c3c2d5193554660c09918a0acba1
Jesper Isacson <isul18wj@student.ju.se>
2021-04-15 15:36:57 +0200

    test function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const express = require('express')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,10 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const app = express()
+
+
+exports.test = functions.https.onRequest((request, response) => {
+    response.send("Hello World!!!!!!!!")
+})
