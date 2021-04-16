a551d628fa3684f5c69e2866c7669d1c0ec0fc9a
Jesper Isacson <isul18wj@student.ju.se>
2021-04-16 14:21:14 +0200

    API can send data

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const express = require('express')
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 admin.initializeApp()
+const db = admin.firestore()
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,6 +16,51 @@ admin.initializeApp()
 const app = express()
 
 
-exports.test = functions.https.onRequest((request, response) => {
-    response.send("Hello World!!!!!!!!")
+app.get("/", async (request, response) =>{
+    const dataRef = await db.collection("DrivingPath")
+    //const query = dataRef.where('Nodes', 'array-contains')
+
+    var node = []
+
+    await dataRef.get().then(snapshot =>{
+        snapshot.docs.forEach(doc =>{
+            console.log(doc.id, doc.data())
+            node.push(doc.id, doc.data())
+        })
+    })
+
+    /*let node = []
+    dataRef.forEach(doc=>{
+        let id = doc.id
+        let data = doc.data()
+
+        node.push[{id, ...data}]
+    })*/
+
+    console.log(node)
+    response.status(200).send(JSON.stringify(node))
 })
+
+exports.test = functions.https.onRequest(app)
+
+
+/*exports.getData = functions.https.onRequest((request, response) =>{
+    var dataRef = db.collection("DrivingPath").get()
+
+    const node = []
+    dataRef.then((snapshot) => {
+
+        snapshot.docs.forEach(doc => {
+            let id = doc.id
+            let data = doc.data()
+            
+        
+            //console.log(id, '=>', data)
+            node.push(JSON.stringify(data))
+            //console.log(node)
+        })
+    })
+    response.status(200).send(node)
+})
+
+app.use()*/
